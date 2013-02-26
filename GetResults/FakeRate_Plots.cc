cefc354e18004229cadabce64642b2a2317a9511
abdollah110 <smohamadi@gmail.com>
2013-02-26 16:33:14 +0100

    macros to make results

---
+++ GetResults/FakeRate_Plots.cc
@@ -0,0 +1,142 @@
+#include "TGraph.h"
+#include "TGraphErrors.h"
+#include <fstream>
+#include <iostream>
+#include <TFile.h>
+#include <ostream>
+#include <time.h>
+#include <TF1.h>
+#include <TPad.h>
+#include <TGraphAsymmErrors.h>
+#include <TPave.h>
+#include <TCanvas.h>
+#include <TAttAxis.h>
+#include "TMath.h"
+
+void myLegend(TGraphAsymmErrors * obj) {
+    TLegend *l = new TLegend(0.50, 0.50, 0.7, 0.7, NULL, "brNDC");
+
+    l->SetBorderSize(0);
+    l->SetFillColor(0);
+    l->SetTextSize(.04);
+    l->AddEntry(obj, "Data 2012 D", "lpf");
+    //    l->AddEntry(Fake_R_12AB, "Data 2012 A+B+C", "lpf");
+    //    l->AddEntry(Fake_R_11, "Data 2011", "lpf");
+
+
+    l->Draw();
+}
+
+void myStyle_Graph(TGraph * TGraph_FR, int color) {
+
+
+    TGraph_FR->SetMarkerColor(color);
+    TGraph_FR->SetMarkerSize(1.4);
+    TGraph_FR->SetMarkerStyle(20 + color);
+    TGraph_FR->SetTitle("");
+    TGraph_FR->GetXaxis()->SetTitle("#tau Pt [GeV]");
+    TGraph_FR->GetXaxis()->SetTitleOffset(1);
+    TGraph_FR->GetXaxis()->SetTitleSize(0.045);
+    TGraph_FR->GetXaxis()->SetLabelSize(0.04);
+    TGraph_FR->GetYaxis()->SetLabelSize(0.03);
+    TGraph_FR->GetYaxis()->SetTitle("Fake Rate");
+    TGraph_FR->GetYaxis()->SetRangeUser(0, 0.101);
+    TGraph_FR->GetXaxis()->SetRangeUser(0, 90);
+    TGraph_FR->GetYaxis()->SetTitleOffset(1.0);
+    TGraph_FR->GetYaxis()->SetTitleSize(0.045);
+}
+
+void myStyle_Fit(TF1* theFit, int color) {
+    theFit->SetRange(10, 100);
+    theFit->SetLineColor(color);
+    theFit->SetLineWidth(2);
+}
+
+Double_t fitFunction(Double_t *x, Double_t *par) {
+    return par[0] + par[1]*(TMath::Exp(par[2] * x[0]));
+}
+
+TF1* fake_Fit(TGraphAsymmErrors * TGraph_FR, int color) {
+
+    const int nPar = 3; // number of parameters in the fit
+    Double_t fitRangeMin = 15,
+            fitRangeMax = 80;
+
+    TF1* theFit = new TF1("theFit", fitFunction, fitRangeMin, fitRangeMax, nPar);
+
+    theFit->SetParameter(0, 0.012);
+    theFit->SetParameter(1, 0.18);
+    theFit->SetParameter(2, -0.095);
+
+    TGraph_FR->Fit("theFit", "R0");
+    myStyle_Fit(theFit, color);
+    return theFit;
+
+
+}
+
+TGraphAsymmErrors* M_FR(std::string files, std::string num, std::string denum, int color) {
+    TFile *inputFile = new TFile(files.c_str());
+
+    TH1D *Numerator = (TH1D*) inputFile->Get(num.c_str());
+    TH1D *Denumerator = (TH1D*) inputFile->Get(denum.c_str());
+
+    TH1D *Num_rebin = (TH1D*) Numerator->Rebin(5);
+    TH1D *Denum_rebin = (TH1D*) Denumerator->Rebin(5);
+    cout << num.c_str() << " = " << Num_rebin->GetEntries() << "/" << Denum_rebin->GetEntries() << "  =  " << Num_rebin->GetEntries()*1.0 / Denum_rebin->GetEntries() << endl;
+
+    TGraphAsymmErrors * TGraph_FR = new TGraphAsymmErrors(Num_rebin, Denum_rebin);
+
+    myStyle_Graph(TGraph_FR, color);
+    return TGraph_FR;
+}
+
+void FakeRate_Plots() {
+
+    //Tau FR
+    TGraphAsymmErrors * loose = M_FR("Mega.root", "FakeRate_TT_Tau_Pt_After_Loose", "FakeRate_TT_Tau_Pt_Before", 2);
+    TF1 * loose_Fit = fake_Fit(loose, 2);
+    TGraphAsymmErrors * medium = M_FR("Mega.root", "FakeRate_TT_Tau_Pt_After_Medium", "FakeRate_TT_Tau_Pt_Before", 3);
+    TF1 * medium_Fit = fake_Fit(medium, 3);
+    TGraphAsymmErrors * tight = M_FR("Mega.root", "FakeRate_TT_Tau_Pt_After_Tight", "FakeRate_TT_Tau_Pt_Before", 4);
+    TF1 * tight_Fit = fake_Fit(tight, 4);
+
+    //    //Electron FR
+    //    TGraphAsymmErrors * loose = M_FR("Mega.root", "FakeRate_ET_Electron_Loose_After_0", "FakeRate_ET_Electron_Pt_Before_0", 2);
+    //    TGraphAsymmErrors * medium = M_FR("Mega.root", "FakeRate_ET_Electron_Tight_After_0", "FakeRate_ET_Electron_Pt_Before_0", 3);
+    //
+    //    //Muon FR
+    //    TGraphAsymmErrors * loose = M_FR("Mega.root", "FakeRate_MT_Muon_Loose_After_0", "FakeRate_MT_Muon_Pt_Before_0", 2);
+    //    TGraphAsymmErrors * medium = M_FR("Mega.root", "FakeRate_MT_Muon_Tight_After_0", "FakeRate_MT_Muon_Pt_Before_0", 3);
+
+
+    TCanvas* canvas = new TCanvas("canvas", "", 600, 600);
+    canvas->SetFillColor(0);
+    canvas->SetTitle("0");
+    canvas->SetBorderMode(0);
+    canvas->SetBorderSize(2);
+    canvas->SetFrameBorderMode(0);
+    canvas->SetFrameLineWidth(2);
+    canvas->SetFrameBorderMode(0);
+
+
+    canvas->cd(1);
+    loose->Draw("PAE");
+    loose_Fit->Draw("SAME");
+    medium->Draw("PSAME");
+    medium_Fit->Draw("SAME");
+    tight->Draw("PSAME");
+    tight_Fit->Draw("SAME");
+
+    TLegend *l = new TLegend(0.60, 0.60, 0.7, 0.7, NULL, "brNDC");
+    l->SetBorderSize(0);
+    l->SetFillColor(0);
+    l->SetTextSize(.04);
+    l->AddEntry(loose,"Tau Loose FR" , "lpf");
+    //    l->AddEntry(Fake_R_12AB, "Data 2012 A+B+C", "lpf");
+    //    l->AddEntry(Fake_R_11, "Data 2011", "lpf");
+    l->Draw();
+
+
+
+}
---
+++ GetResults/make_Results_2DTable.py
@@ -0,0 +1,80 @@
+#! /usr/bin/python
+
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+#http://root.cern.ch/viewvc/trunk/tutorials/pyroot/hsimple.py?revision=20881&view=markup
+__author__ = "abdollahmohammadi"
+__date__ = "$Feb 23, 2013 10:39:33 PM$"
+
+import math
+import ROOT
+from ROOT import Double
+from ROOT import TCanvas
+from ROOT import TFile
+from ROOT import TH1F
+from ROOT import TH2F
+from ROOT import TNtuple
+from ROOT import TProfile
+from ROOT import gBenchmark
+from ROOT import gROOT
+from ROOT import gRandom
+from ROOT import gSystem
+
+gROOT.Reset()
+import os
+ROOT.gROOT.SetBatch(True)
+#ROOT.gROOT.ProcessLine('.x rootlogon.C')
+DIR_ROOT = 'Root_8TeV/'
+luminosity = 19242
+def XSection(mX):
+    if mX == 110: return  [.00471, .00206]
+    if mX == 115:  return [.00392, .00336]
+    if mX == 120:  return [.00319, .00479]
+    if mX == 125:  return [.00251, .00640]
+    if mX == 130:  return [.00191, .00789]
+    if mX == 135:  return [.00139, .00917]
+    if mX == 140:  return [.00097, .0101]
+    if mX == 145:  return [.00064, .0142]
+    if mX == 'ZZ4L': return 0.130
+    if mX == 'Data': return 1
+
+if __name__ == "__main__":
+
+    myOut = TFile('FullResults.root', 'RECREATE')
+    FullResults  = TH2F('FullResults', 'FullResults', 10, 0, 10, 20, 0, 20)
+    channel = ["mmtt", "mmet", "mmmt", "mmme", "eett", "eemt", "eeet", "eeem"]
+    signal = ['zhtt', 'zhww']
+    mass = [110, 115, 120, 125, 130, 135, 140, 145]
+    BackGround = ['ZZ4L', 'Data']
+    lenghtSig = len(signal) * len(mass)
+    
+    for chl in range(len(channel)):
+        ###################################### Filling Signal ZH and WH ########
+        for sig in range(len(signal)):
+            for m in range(len(mass)):#    for m in range(110, 145, 5):
+                myfile = TFile(DIR_ROOT + str(signal[sig]) + str(mass[m]) + '.root')
+                Histo = myfile.Get(str(channel[chl]))
+                value = Histo.GetBinContent(3) * luminosity * XSection(mass[m])[sig] / Histo.GetBinContent(1)
+                FullResults.SetBinContent(chl + 1, sig * len(mass) + m + 1, value)
+                FullResults.Fill(9, sig * len(mass) + m, value)
+                FullResults.GetYaxis().SetBinLabel(sig * len(mass) + m + 1, str(signal[sig]) + str(mass[m]))
+        #######################################  Filling Reducible BG ##########
+        myfile = TFile('Reducible.root')
+        Histo = myfile.Get('histo_Reducible')
+        value = Histo.GetBinContent(chl + 1)
+        FullResults.SetBinContent(chl + 1, lenghtSig  + 2, value)
+        FullResults.Fill(9, lenghtSig + 1, value)
+        FullResults.GetYaxis().SetBinLabel(lenghtSig  + 2, 'Reducible')
+        #######################################   Filling BG and Data  #########
+        for bg in range (len(BackGround)):
+            myfile = TFile(DIR_ROOT + str(BackGround[bg]) + '.root')
+            Histo = myfile.Get(str(channel[chl]))
+            value = Histo.GetBinContent(3) * luminosity * XSection(BackGround[bg]) / Histo.GetBinContent(1)
+            if BackGround[bg] == 'Data':
+                value = Histo.GetBinContent(3)
+            FullResults.SetBinContent(chl + 1, lenghtSig + bg + 3, value)
+            FullResults.Fill(9, lenghtSig + bg + 2, value)
+            FullResults.GetYaxis().SetBinLabel(lenghtSig  + bg + 3, str(BackGround[bg]))
+        ########################################################################
+        FullResults.GetXaxis().SetBinLabel(chl + 1, channel[chl])
+    myOut.Write()

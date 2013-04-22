17a2718acaae5a56434a00ad1b30b5355246bc51
abdollah110 <smohamadi@gmail.com>
2013-04-22 14:01:55 +0200

    resolve difference between trees

--- Batch_Submit.py
+++ Batch_Submit.py
@@ -11,25 +11,36 @@ import shutil
 
     ########################################################################################
 Run_Over = {
-    1:("/pnfs/iihe/cms/store/user/ccaillol/ZHttNtuples_v2_Filtered/42X/Data", "data11", "Ele", "25:00:00"),
-    2:("/pnfs/iihe/cms/store/user/ccaillol/ZHttNtuples_v2_Filtered/42X/Data", "data11", "Mu",  "25:00:00"),
-    3:("/pnfs/iihe/cms/store/user/lperrini/FILTER2/ZHttNtuples_v2/42X/Data", "data11", "Ele", "25:00:00"),
-    4:("/pnfs/iihe/cms/store/user/lperrini/FILTER2/ZHttNtuples_v2/42X/Data", "data11", "Mu",  "25:00:00"),
+
+    ##### Unfiltered
+    1:("/pnfs/iihe/cms/store/user/ccaillol/ZHttNtuples_v2/53X/Data", "data11", "Ele", "25:00:00"),
+    2:("/pnfs/iihe/cms/store/user/ccaillol/ZHttNtuples_v2/53X/Data", "data11", "Mu",  "25:00:00"),
+    3:("/pnfs/iihe/cms/store/user/lperrini/ZHttNtuples_v2/42X/Data", "data11", "Ele", "25:00:00"),
+    4:("/pnfs/iihe/cms/store/user/lperrini/ZHttNtuples_v2/42X/Data", "data11", "Mu",  "25:00:00"),
+    5:("/pnfs/iihe/cms/store/user/abdollah/ZHttNtuples_v2/53X/Data", "data12", "Ele", "25:00:00"),
+    6:("/pnfs/iihe/cms/store/user/abdollah/ZHttNtuples_v2/53X/Data", "data12", "Mu", "25:00:00"),
+    7:("/pnfs/iihe/cms/store/user/jez/ZHttNtuples_v2/53X/Data", "data12", "Ele", "25:00:00"),
+    8:("/pnfs/iihe/cms/store/user/jez/ZHttNtuples_v2/53X/Data", "data12", "Mu", "25:00:00"),
+    9:("/pnfs/iihe/cms/store/user/ccaillol/ZHttNtuples_v2/53X/MC", "mc11", "Tot", "25:00:00"),
+    10:("/pnfs/iihe/cms/store/user/lperrini/ZHttNtuples_v2/42X/MC", "mc11", "Tot",  "45:00:00"),
+    11:("/pnfs/iihe/cms/store/user/abdollah/ZHttNtuples_v2/53X/MC", "mc12", "Tot", "25:00:00"),
+    12:("/pnfs/iihe/cms/store/user/jez/ZHttNtuples_v2/53X/MC", "mc12", "Tot", "45:00:00"),
+
     
-    5:("/pnfs/iihe/cms/store/user/ccaillol/FILTER2/ZHttNtuples_v2/53X/Data", "data12", "Ele", "25:00:00"),
-    6:("/pnfs/iihe/cms/store/user/ccaillol/FILTER2/ZHttNtuples_v2/53X/Data", "data12", "Mu", "25:00:00"),
-    7:("/pnfs/iihe/cms/store/user/jez/FILTER2/ZHttNtuples_v2/53X/Data", "data12", "Ele", "25:00:00"),
-    8:("/pnfs/iihe/cms/store/user/jez/FILTER2/ZHttNtuples_v2/53X/Data", "data12", "Mu", "25:00:00"),
+   ##### Filtered
+    1:("/pnfs/iihe/cms/store/user/ccaillol/ZHttNtuples_v2_Filtered/42X/Data", "data11", "Ele", "15:00:00"),
+    2:("/pnfs/iihe/cms/store/user/ccaillol/ZHttNtuples_v2_Filtered/42X/Data", "data11", "Mu",  "15:00:00"),
+    3:("/pnfs/iihe/cms/store/user/lperrini/FILTER2/ZHttNtuples_v2/42X/Data", "data11", "Ele", "15:00:00"),
+    4:("/pnfs/iihe/cms/store/user/lperrini/FILTER2/ZHttNtuples_v2/42X/Data", "data11", "Mu",  "15:00:00"),
+    5:("/pnfs/iihe/cms/store/user/ccaillol/FILTER2/ZHttNtuples_v2/53X/Data", "data12", "Ele", "15:00:00"),
+    6:("/pnfs/iihe/cms/store/user/ccaillol/FILTER2/ZHttNtuples_v2/53X/Data", "data12", "Mu", "15:00:00"),
+    7:("/pnfs/iihe/cms/store/user/jez/FILTER2/ZHttNtuples_v2/53X/Data", "data12", "Ele", "15:00:00"),
+    8:("/pnfs/iihe/cms/store/user/jez/FILTER2/ZHttNtuples_v2/53X/Data", "data12", "Mu", "15:00:00"),
+    9:("/pnfs/iihe/cms/store/user/ccaillol/ZHttNtuples_v2/53X/MC", "mc11", "Tot", "15:00:00"),
+    10:("/pnfs/iihe/cms/store/user/lperrini/FILTER2/ZHttNtuples_v2/42X/MC", "mc11", "Tot",  "25:00:00"),
+    11:("/pnfs/iihe/cms/store/user/abdollah/ZHttNtuples_v2/53X/MC", "mc12", "Tot", "15:00:00"),
+    12:("/pnfs/iihe/cms/store/user/jez/FILTER2/ZHttNtuples_v2/53X/MC", "mc12", "Tot", "25:00:00"),
     
-   # 9:("/pnfs/iihe/cms/store/user/ccaillol/ZHttNtuples_v2/53X/MC", "mc11", "Tot", "25:00:00"),
-   # 10:("/pnfs/iihe/cms/store/user/lperrini/ZHttNtuples_v2/42X/MC", "mc11", "Tot",  "25:00:00"),
-    
-   # 11:("/pnfs/iihe/cms/store/user/abdollah/ZHttNtuples_v2/53X/MC", "mc12", "Tot", "25:00:00"),
-   # 12:("/pnfs/iihe/cms/store/user/jez/ZHttNtuples_v2/53X/MC", "mc12", "Tot", "25:00:00"),
-    
-   # 5:("/pnfs/iihe/cms/store/user/jez/ZHttNtuples/42X/MC", "mc11", "Tot", "05:00:00"),
-   # 6:("/pnfs/iihe/cms/store/user/abdollah/ZHttNtuples/53X/MC", "mc12", "Tot", "03:00:00"),
-   # 7:("/pnfs/iihe/cms/store/user/jez/ZHttNtuples/53X/MC", "mc12", "Tot", "05:00:00"),
 }
 
     ########################################################################################
